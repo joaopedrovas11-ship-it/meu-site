bbd8032c535b31661c81abd2678cd25a84492b6b
JpPlayzTV <joaopedrovas11@gmail.com>
2026-09-06 06:25:04 -0300

    Configure app to use environment port settings

--- app.py
+++ app.py
@@ -76,5 +76,8 @@ def checkout(produto_id):
         
     return render_template('sucesso.html', cliente=nome_cliente, produto=produto, whatsapp=whatsapp)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
